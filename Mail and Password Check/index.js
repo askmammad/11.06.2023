8e94e4bdfc6942dac5584d210c9a3ef974d62388
askmammad <mammadha@code.edu.az>
2023-06-09 15:54:30 +0400

    second commit

--- Mail and Password Check/index.js
+++ Mail and Password Check/index.js
@@ -5,19 +5,20 @@ let email = document.getElementById("exampleInputEmail1");
 let password = document.getElementById("exampleInputPassword1");
 let myEmail = "askmammad@gmail.com";
 let myPassword = "123456789";
-console.log("Hello");
+let modalText = document.getElementById("modal_text");
 
 form.addEventListener("submit", (e) => checkValue(e));
 
 function checkValue(e){
     let emailVal = email.value;
     let passwordVal = password.value;
+    modalText.innerText="";
     if(emailVal==myEmail && passwordVal==myPassword){
-        alert("Email and password are correct");
+        modalText.innerText = "Password and email are correct. Success!!!";
         form.reset();
     }
     else{
-        alert("Email and password are incorrect. Please try again");
+        modalText.innerText = "Password and email are incorrect. Please try again.";
         form.reset();
     }
     e.preventDefault();
